bae08f2b2700493451efd6b8659c56e15eac2d3b
amirtavallaienejad <111754975+amirtavallaienejad@users.noreply.github.com>
2023-01-17 17:52:21 +0000

    with evaluation

--- Cracks with Xception.py
+++ Cracks with Xception.py
@@ -191,7 +191,101 @@ fig.show()
 
 
 
+# EVALUATIONNNNNNNNNNNNNNNN
 
+
+
+
+
+
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+from pathlib import Path
+from sklearn.model_selection import train_test_split
+
+import tensorflow as tf
+
+from sklearn.metrics import confusion_matrix, classification_report
+
+
+positive_dir = Path(r'C:\Users\up202111331\Desktop\2nd Semester\Machine Learning\Second Report\Concrete Crack Images\Positive')
+negative_dir = Path(r'C:\Users\up202111331\Desktop\2nd Semester\Machine Learning\Second Report\Concrete Crack Images\Negative')
+
+# Creating DataFrames
+
+
+def generate_df(image_dir, label):
+    filepaths = pd.Series(list(image_dir.glob(r'*.jpg')), name='Filepath').astype(str)
+    labels = pd.Series(label, name='Label', index=filepaths.index)
+    df = pd.concat([filepaths, labels], axis=1)
+    return df
+
+
+positive_df = generate_df(positive_dir, label="POSITIVE")
+negative_df = generate_df(negative_dir, label="NEGATIVE")
+
+all_df = pd.concat([positive_df, negative_df], axis=0).sample(frac=1.0, random_state=1).reset_index(drop=True)
+
+print(all_df)
+
+from sklearn.model_selection import train_test_split
+
+train_df, test_df = train_test_split(
+    all_df.sample(6000, random_state=1),
+    train_size=0.7,
+    shuffle=True,
+    random_state=1
+)
+
+train_gen = tf.keras.preprocessing.image.ImageDataGenerator(
+    rescale=1./255,
+    validation_split=0.2
+)
+
+test_data = train_gen.flow_from_dataframe(
+    test_df,
+    x_col='Filepath',
+    y_col='Label',
+    target_size=(120, 120),
+    color_mode='rgb',
+    class_mode='binary',
+    batch_size=32,
+    shuffle=False,
+    seed=42
+)
+
+def evaluate_model(model, test_data):
+    
+    results = model.evaluate(test_data, verbose=0)
+    loss = results[0]
+    acc = results[1]
+    
+    print("    Test Loss: {:.5f}".format(loss))
+    print("Test Accuracy: {:.2f}%".format(acc * 100))
+    
+    y_pred = np.squeeze((model.predict(test_data) >= 0.5).astype(np.int))
+    cm = confusion_matrix(test_data.labels, y_pred)
+    clr = classification_report(test_data.labels, y_pred, target_names=["NEGATIVE", "POSITIVE"])
+    
+    plt.figure(figsize=(6, 6))
+    sns.heatmap(cm, annot=True, fmt='g', vmin=0, cmap='Blues', cbar=False)
+    plt.xticks(ticks=np.arange(2) + 0.5, labels=["NEGATIVE", "POSITIVE"])
+    plt.yticks(ticks=np.arange(2) + 0.5, labels=["NEGATIVE", "POSITIVE"])
+    plt.xlabel("Predicted")
+    plt.ylabel("Actual")
+    plt.title("Confusion Matrix")
+    plt.show()
+    
+    print("Classification Report:\n----------------------\n", clr)
+
+
+evaluate_model(model, test_data)
 
 
 
