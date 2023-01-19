73b157c0139931f1d5187ceae24422e9d7615da8
amirtavallaienejad <111754975+amirtavallaienejad@users.noreply.github.com>
2023-01-19 03:12:05 +0000

    New version of it

--- Cracks with ResNet50.py
+++ Cracks with ResNet50.py
@@ -1,11 +1,187 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan 16 23:52:06 2023
+Created on Wed Jan 18 22:32:10 2023
+
+@author: up202111331
+"""
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+# Input data files are available in the read-only "../input/" directory
+# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+
+import os
+for dirname, _, filenames in os.walk(r'C:\Users\up202111331\Desktop\2nd Semester\Machine Learning\Second Report\Concrete Crack Images'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
+# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
+
+from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras import Model,layers
+import tensorflow as tf
+import keras
+
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+import cv2
+
+def load_images_from_folder(folder):
+    images = []
+    for filename in os.listdir(folder):
+        img = cv2.imread(os.path.join(folder,filename))
+        if img is not None:
+            images.append(img)
+        if len(images)>3:
+            break
+    fig=plt.figure(figsize=(10,12))
+    xrange=range(1,5)
+    
+    for img,x in zip(images,xrange):
+        ax=fig.add_subplot(2,2,x)
+        ax.imshow(img)
+        ax.set_title(img.shape)
+
+# Define a Callback class that stops training once accuracy reaches 99.9%
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if(logs.get('accuracy')>0.999):
+            print("\nReached 99.9% accuracy so cancelling training!")
+            self.model.stop_training = True
+
+
+
+
+
+
+from tensorflow.keras.applications import ResNet50
+
+from keras.models import Sequential
+from keras.layers import Dense
+
+
+train_datagen = ImageDataGenerator(validation_split=0.3) # don't use rescale = 1./255
+
+train_generator = train_datagen.flow_from_directory(r'C:\Users\up202111331\Desktop\2nd Semester\Machine Learning\Second Report\Concrete Crack Images',
+                                                     target_size=(224,224),
+                                                     batch_size=64,
+                                                     shuffle=True,
+                                                     class_mode='categorical',
+                                                     subset='training')
+
+validation_datagen = ImageDataGenerator(validation_split=0.3)
+
+validation_generator =  validation_datagen.flow_from_directory(r'C:\Users\up202111331\Desktop\2nd Semester\Machine Learning\Second Report\Concrete Crack Images',
+                                                                target_size=(224,224),
+                                                                batch_size=64,
+                                                                class_mode='categorical',
+                                                                subset='validation')
+
+model_res50 = Sequential()
+
+model_res50.add(ResNet50(
+    include_top=False,
+    pooling='avg',
+    weights='imagenet'
+    ))
+
+model_res50.add(Dense(2, activation='softmax'))
+
+model_res50.layers[0].trainable = False 
+
+model_res50.summary()
+
+steps_per_epoch_training = len(train_generator)
+steps_per_epoch_validation = len(validation_generator)
+
+
+callbacks = myCallback()
+
+model_res50.compile(optimizer='adam', 
+              loss='categorical_crossentropy', 
+              metrics=['accuracy'])
+
+
+fit_history = model_res50.fit_generator(
+    train_generator,
+    steps_per_epoch=steps_per_epoch_training,
+    validation_steps=steps_per_epoch_validation,
+    epochs=7,
+    validation_data=validation_generator,
+    verbose=1,
+    callbacks=[callbacks]
+)
+
+acc = fit_history.history['accuracy']
+val_acc = fit_history.history['val_accuracy']
+loss = fit_history.history['loss']
+val_loss = fit_history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'r', label='Training accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+
+plt.figure()
+
+plt.plot(epochs, loss, 'r', label='Training Loss')
+plt.plot(epochs, val_loss, 'b', label='Validation Loss')
+plt.title('Training and validation loss')
+
+plt.legend()
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 18 13:51:23 2023
 
 @author: up202111331
 """
 
-import tensorflow.keras as K
 
 import numpy as np
 import pandas as pd
@@ -19,7 +195,13 @@ from sklearn.model_selection import train_test_split
 
 import tensorflow as tf
 
+from keras.applications import vgg16
+from keras.layers import Dense,Flatten
+from keras.models import Model
+
 from sklearn.metrics import confusion_matrix, classification_report
+
+
 
 positive_dir = Path(r'C:\Users\up202111331\Desktop\2nd Semester\Machine Learning\Second Report\Concrete Crack Images\Positive')
 negative_dir = Path(r'C:\Users\up202111331\Desktop\2nd Semester\Machine Learning\Second Report\Concrete Crack Images\Negative')
@@ -68,7 +250,7 @@ train_data = train_gen.flow_from_dataframe(
     train_df,
     x_col='Filepath',
     y_col='Label',
-    target_size=(120, 120),
+    target_size=(224,224),
     color_mode='rgb',
     class_mode='binary',
     batch_size=32,
@@ -81,7 +263,7 @@ val_data = train_gen.flow_from_dataframe(
     train_df,
     x_col='Filepath',
     y_col='Label',
-    target_size=(120, 120),
+    target_size=(224,224),
     color_mode='rgb',
     class_mode='binary',
     batch_size=32,
@@ -94,7 +276,7 @@ test_data = train_gen.flow_from_dataframe(
     test_df,
     x_col='Filepath',
     y_col='Label',
-    target_size=(120, 120),
+    target_size=(224,224),
     color_mode='rgb',
     class_mode='binary',
     batch_size=32,
@@ -104,66 +286,39 @@ test_data = train_gen.flow_from_dataframe(
 
 
 
-# Training
-
-input_t = K.Input(shape = (120,120,3))
-res_model = K.applications.ResNet50(include_top = False, weights = 'imagenet', input_tensor = input_t)
-
-for layer in res_model.layers[:143]:
-    layer.trainable = False
-    
-    
-for i, layer in enumerate(res_model.layers):
-    print(i,layer.name,"-",layer.trainable)
-    
-    
-model = K.models.Sequential()
-model.add(res_model)
-model.add(K.layers.Flatten())
-model.add(K.layers.Dense(1, activation = 'sigmoid'))
 
 
 
-model.compile(loss = 'categorical_crossentropy', optimizer = K.optimizers.RMSprop(lr=2e-5), metrics = ['accuracy'])
-history = model.fit(
-    train_data,
-    validation_data=val_data,
-    epochs=100,
-    batch_size = 32,
-    verbose = 1
-)
-fig = px.line(
-    history.history,
-    y=['loss', 'val_loss'],
-    labels={'index': "Epoch", 'value': "Loss"},
-    title="Training and Validation Loss Over Time"
-)
-
-fig.show()
-    
-def evaluate_model(model, test_data):
-    
-    results = model.evaluate(test_data, verbose=0)
-    loss = results[0]
-    acc = results[1]
-    
-    print("    Test Loss: {:.5f}".format(loss))
-    print("Test Accuracy: {:.2f}%".format(acc * 100))
-    
-    y_pred = np.squeeze((model.predict(test_data) >= 0.5).astype(np.int))
-    cm = confusion_matrix(test_data.labels, y_pred)
-    clr = classification_report(test_data.labels, y_pred, target_names=["NEGATIVE", "POSITIVE"])
-    
-    plt.figure(figsize=(6, 6))
-    sns.heatmap(cm, annot=True, fmt='g', vmin=0, cmap='Blues', cbar=False)
-    plt.xticks(ticks=np.arange(2) + 0.5, labels=["NEGATIVE", "POSITIVE"])
-    plt.yticks(ticks=np.arange(2) + 0.5, labels=["NEGATIVE", "POSITIVE"])
-    plt.xlabel("Predicted")
-    plt.ylabel("Actual")
-    plt.title("Confusion Matrix")
-    plt.show()
-    
-    print("Classification Report:\n----------------------\n", clr)
 
 
-evaluate_model(model, test_data)
+
+
+
+
+
+
+y_pred = np.squeeze((model_res50.predict(test_data) >= 0.5).astype(np.int))
+mistake_idx = (y_pred != test_data.labels).nonzero()[0]
+print(len(mistake_idx), "mistakes.")
+print("Indices:", mistake_idx)
+
+
+# Display the detection mistakes
+plt.figure(figsize=(20, 10))
+
+for i, idx in enumerate(mistake_idx):
+    
+    # Get batch number and image number (batch of 32 images)
+    batch = idx // 32
+    image = idx % 32
+    
+    plt.subplot(6,6, i+1)
+    plt.imshow(test_data[batch][0][image])
+    plt.title("No crack detected" if y_pred[idx] == 0 else "Crack detected", color='red')
+    plt.axis('off')
+    
+plt.suptitle("Detection Mistakes in MobileNet network", fontsize=20)
+plt.show()
+
+
+
