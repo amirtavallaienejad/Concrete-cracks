ac088e8cadb6bbbf5c8a240fa0ca4fbd172a7edd
amirtavallaienejad <111754975+amirtavallaienejad@users.noreply.github.com>
2023-01-19 02:50:02 +0000

    Final version

--- Cracks with VGG16.py
+++ Cracks with VGG16.py
@@ -1,55 +1,115 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan 16 21:52:49 2023
+Created on Wed Jan 18 13:51:23 2023
 
 @author: up202111331
 """
 
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+from pathlib import Path
+from sklearn.model_selection import train_test_split
+
+import tensorflow as tf
+
+from keras.applications import vgg16
 from keras.layers import Dense,Flatten
 from keras.models import Model
-from keras.applications import vgg16
-from keras.preprocessing.image import ImageDataGenerator
-import tensorflow as tf
-from glob import glob
-import numpy as np
-import matplotlib.pyplot as plt
+
 from sklearn.metrics import confusion_matrix, classification_report
 
-import os
-
-root_dir=r'C:\Users\up202111331\Desktop\2nd Semester\Machine Learning\Second Report\Concrete Crack Images'
-
-for dirname, _, filenames in os.walk(root_dir):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
 
 
+positive_dir = Path(r'C:\Users\up202111331\Desktop\2nd Semester\Machine Learning\Second Report\Concrete Crack Images\Positive')
+negative_dir = Path(r'C:\Users\up202111331\Desktop\2nd Semester\Machine Learning\Second Report\Concrete Crack Images\Negative')
+
+# Creating DataFrames
 
 
-data_gen = ImageDataGenerator(rotation_range=30 ,
-                              horizontal_flip=True, 
-                              vertical_flip = True,
-                              rescale = 1/255.0,
-                             validation_split=0.1)
+def generate_df(image_dir, label):
+    filepaths = pd.Series(list(image_dir.glob(r'*.jpg')), name='Filepath').astype(str)
+    labels = pd.Series(label, name='Label', index=filepaths.index)
+    df = pd.concat([filepaths, labels], axis=1)
+    return df
 
-train_generator = data_gen.flow_from_directory(root_dir,
-                                              target_size = (224,224),
-                                              batch_size = 16,
-                                              class_mode = 'binary',
-                                               color_mode = 'rgb',
-                                               subset='training',shuffle=True
-                                              )
 
-valid_generator = data_gen.flow_from_directory(root_dir,
-                                              target_size = (224,224),
-                                              batch_size = 16,
-                                              class_mode = 'binary',
-                                               color_mode = 'rgb',
-                                               subset='validation',shuffle=True
-                                              )
+positive_df = generate_df(positive_dir, label="POSITIVE")
+negative_df = generate_df(negative_dir, label="NEGATIVE")
 
+all_df = pd.concat([positive_df, negative_df], axis=0).sample(frac=1.0, random_state=1).reset_index(drop=True)
+
+print(all_df)
+
+
+
+
+
+train_df, test_df = train_test_split(
+    all_df.sample(6000, random_state=1),
+    train_size=0.7,
+    shuffle=True,
+    random_state=1
+)
+
+
+# Loading Image Data
+
+train_gen = tf.keras.preprocessing.image.ImageDataGenerator(
+    rescale=1./255,
+    validation_split=0.2
+)
+
+test_gen = tf.keras.preprocessing.image.ImageDataGenerator(
+    rescale=1./255
+)
+
+train_data = train_gen.flow_from_dataframe(
+    train_df,
+    x_col='Filepath',
+    y_col='Label',
+    target_size=(224,224),
+    color_mode='rgb',
+    class_mode='binary',
+    batch_size=32,
+    shuffle=True,
+    seed=42,
+    subset='training'
+)
+
+val_data = train_gen.flow_from_dataframe(
+    train_df,
+    x_col='Filepath',
+    y_col='Label',
+    target_size=(224,224),
+    color_mode='rgb',
+    class_mode='binary',
+    batch_size=32,
+    shuffle=True,
+    seed=42,
+    subset='validation'
+)
+
+test_data = train_gen.flow_from_dataframe(
+    test_df,
+    x_col='Filepath',
+    y_col='Label',
+    target_size=(224,224),
+    color_mode='rgb',
+    class_mode='binary',
+    batch_size=32,
+    shuffle=False,
+    seed=42
+)
+
+
+
+# Training 
 
 
 IMAGE_SIZE = [224,224]
@@ -71,26 +131,130 @@ model = Model(inputs=vgg_16.input , outputs=pred)
 
 ## Let's compile our model
 model.compile(optimizer='adam',
-             loss='binary_crossentropy',
-             metrics=['accuracy'])
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
 
 model.summary()
 
 
 ## Fit the Model
-result = model.fit(train_generator,
-                   validation_data=valid_generator,
-                   epochs=10,
-                   callbacks=[
+result = model.fit(train_data,
+                    validation_data=val_data,
+                    epochs=30,
+                    callbacks=[
                         tf.keras.callbacks.EarlyStopping(
             monitor='val_loss',
             patience=3,
             restore_best_weights=True
         )
-                   ]
+                    ]
                     )
+history = model.fit(
+    train_data,
+    validation_data=val_data,
+    epochs=30,
+    batch_size = 32,
+    verbose = 1
+)
+
+
+
+#visualizing
+
+
+
+fig = px.line(
+    history.history,
+    y=['loss', 'val_loss'],
+    labels={'index': "Epoch", 'value': "Loss"},
+    title="Training and Validation Loss Over Time"
+)
+
+fig.show()
+    
+def evaluate_model(model, test_data):
+    
+    results = model.evaluate(test_data, verbose=0)
+    loss = results[0]
+    acc = results[1]
+    
+    print("    Test Loss: {:.5f}".format(loss))
+    print("Test Accuracy: {:.2f}%".format(acc * 100))
+    
+    y_pred = np.squeeze((model.predict(test_data) >= 0.5).astype(np.int))
+    cm = confusion_matrix(test_data.labels, y_pred)
+    clr = classification_report(test_data.labels, y_pred, target_names=["NEGATIVE", "POSITIVE"])
+    
+    plt.figure(figsize=(6, 6))
+    sns.heatmap(cm, annot=True, fmt='g', vmin=0, cmap='Blues', cbar=False)
+    plt.xticks(ticks=np.arange(2) + 0.5, labels=["NEGATIVE", "POSITIVE"])
+    plt.yticks(ticks=np.arange(2) + 0.5, labels=["NEGATIVE", "POSITIVE"])
+    plt.xlabel("Predicted")
+    plt.ylabel("Actual")
+    plt.title("Confusion Matrix of VGG16 Network")
+    plt.show()
+    
+    print("Classification Report:\n----------------------\n", clr)
+
+
+evaluate_model(model, test_data)
 
 
 
 
+# detecing the incorrect data
 
+
+
+y_pred = np.squeeze((model.predict(test_data) >= 0.5).astype(np.int))
+mistake_idx = (y_pred != test_data.labels).nonzero()[0]
+print(len(mistake_idx), "mistakes.")
+print("Indices:", mistake_idx)
+
+
+# Display the detection mistakes
+plt.figure(figsize=(20, 10))
+
+for i, idx in enumerate(mistake_idx):
+    
+    # Get batch number and image number (batch of 32 images)
+    batch = idx // 32
+    image = idx % 32
+    
+    plt.subplot(6,6, i+1)
+    plt.imshow(test_data[batch][0][image])
+    plt.title("No crack detected" if y_pred[idx] == 0 else "Crack detected", color='red')
+    plt.axis('off')
+    
+plt.suptitle("Detection Mistakes in VGG16 network", fontsize=20)
+plt.show()
+
+
+# evaluation the performance
+
+
+import matplotlib.pyplot as plt
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+# x-axis: epochs, y-axis: acc
+plt.plot(epochs, acc, 'r', label='Trainig accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+
+plt.legend()
+
+plt.show()
+
+plt.plot(epochs, loss, 'r', label='Training Loss')
+plt.plot(epochs, val_loss, 'b', label='Validation Loss')
+plt.title('Training and validation loss')
+
+plt.legend()
+
+plt.show()
